e00da003c5c4fcd2af0bb82fc4d3c114167891cf
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-09-30 11:25:56 +0900

    Update versions of starting installations

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const HtmlWebPackPlugin = require('html-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
