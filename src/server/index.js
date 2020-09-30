580df09cdf321622afb66ddb5b3789fc05d63ae1
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-09-30 20:09:59 +0900

    Add API key in environment variable .env file

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,7 @@
+const dotenv = require('dotenv')
+dotenv.config()
+const API_KEY = process.env.API_KEY
+
 const path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -7,8 +11,6 @@ const app = express()
 
 app.use(cors())
 app.use(express.static('dist'))
-
-console.log(__dirname)
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
