85d45782cd3eea898a8dc4ec91f82b110e01cd24
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-10-02 14:44:09 +0900

    Finish comments

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -1,11 +1,13 @@
+// Receives the click event
 export function handleSubmit(event) {
     event.preventDefault()
 
+    // Clears the result fields in case of repeat use
     document.getElementById('results').innerHTML = ""
     document.getElementById('subjectivity').innerHTML = ""
     document.getElementById('score').innerHTML = ""
 
-    // check what text was put into the form field
+    // Validate the user input into the form field
     let userInput = document.getElementById('inputURL').value
     const errorElement = document.getElementById('errorMessage')
     if (!Client.checkURL(userInput)) {
@@ -18,12 +20,15 @@ export function handleSubmit(event) {
 
     console.log("::: Form Submitted :::")
 
+    // Calls function to start the API call in server
     getAnalysis('http://localhost:8081/call', userInput)
 }
 
+// Post route to server which will do API call
 async function getAnalysis(url, userInput) {
 
     await fetch(url, {
+        // Must use POST. GET can't have a body, so can't send URL to server
         method: 'POST',
         credentials: 'same-origin',
         headers: {
@@ -32,29 +37,40 @@ async function getAnalysis(url, userInput) {
         // Body data type must match "Content-Type" header        
         body: userInput,
     })
+        // Convert response to JSON, call updateUI
         .then(res => res.json())
         .then(function (res) {
             updateUI(res)
         })
 }
 
+// Updates the UI so user can see the result of analysis
 function updateUI(res) {
 
+    // Find the elements
     const resultsElement = document.getElementById('results')
     const subjectivityElement = document.getElementById('subjectivity')
     const scoreElement = document.getElementById('score')
 
+    // Update UI with the status message from the server
     resultsElement.innerHTML = `Status - ${res.message}`
+
+    // If result was good, (contains a status key), update the rest of 
+    // the UI elements
     if ("status" in res) {
         console.log('good data, should update')
         let subjectivity = res.subjectivity
         subjectivity = subjectivity.toLowerCase()
         subjectivity = subjectivity[0].toUpperCase() + subjectivity.substring(1);
         subjectivityElement.innerHTML = `Subjectivity - ${subjectivity}`
+
+        // This score element uses the result of the function call
+        // to scoreDescription
         scoreElement.innerHTML = `Positivity - ${scoreDescription(res.score_tag)}`
     }
 }
 
+// Takes the score returned from API and converts it to English
 export function scoreDescription(score) {
     switch (score) {
         case "P+":
