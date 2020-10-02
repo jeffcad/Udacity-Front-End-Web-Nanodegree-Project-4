f122158a6f67c21dc11b4341b48e786462155941
Jeff Cadieux <jeffcadieux@hotmail.com>
2020-10-02 14:20:44 +0900

    CSS update

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,3 +1,6 @@
+// Checks that the user input starts with http:// or https://
+// and has no whitespace
+// Uses regular expression as test condition
 export function checkURL(userInput) {
     console.log("::: Running checkURL :::", userInput)
     const httpCheck = /^http:\/\/|^https:\/\//i
